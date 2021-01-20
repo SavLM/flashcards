a6ee0e7b8cd0922358fc48953eadfe382a8bbeda
SavLM <50849597+SavLM@users.noreply.github.com>
2021-01-20 15:27:31 -0500

    trying something

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ var index = 0;
 
 function tryingsomething() {
     jQuery.get('terms.txt', function(data) {
-      deck = data.split("\n");
+      deck = data.val.split("\n");
     });
     alert(deck[0][1])
 }
